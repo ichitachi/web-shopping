f17638ba68d62a398033ac951429f0992ff8650a
son.nguyen-truong <son.nguyen-truong@banvien.com>
2019-04-27 00:39:14 +0700

    init code v2.1 config

--- shopping-application/src/main/java/tikuka/olt/org/ShoppingApplication.java
+++ shopping-application/src/main/java/tikuka/olt/org/ShoppingApplication.java
@@ -2,8 +2,12 @@ package tikuka.olt.org;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.client.test.OAuth2ContextConfiguration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@OAuth2ContextConfiguration
+@EnableResourceServer
 public class ShoppingApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingApplication.class, args);
